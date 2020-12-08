a29ff282d28dac56ca65dafd6fb6507ee6f033fc
Lord-Gusarov <2367@holbertonschool.com>
2020-12-08 14:20:30 -0400

    forcing the use of .format(...)

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,7 +3,7 @@ i = 0
 dif = ord('A') - ord('a')
 for l in range(ord('z'), ord('a') - 1, -1):
     if i % 2 == 1:
-        print(chr(l + dif), end="")
+        print("{}".format(chr(l + dif)), end="")
     else:
-        print(chr(l), end="")
+        print("{}".format(chr(l)), end="")
     i += 1
