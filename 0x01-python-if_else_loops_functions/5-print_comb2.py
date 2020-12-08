1cd46c56e6a16038cd5d0b4d55dfa5e847c8e95f
Lord-Gusarov <2367@holbertonschool.com>
2020-12-08 14:08:10 -0400

    one less print statement

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    print("{:02d}".format(i), end="")
-    if i < 99:
-        print(", ", end="")
-print("")
+for i in range(0, 99):
+    print("{:02d}, ".format(i), end="")
+print("99")
