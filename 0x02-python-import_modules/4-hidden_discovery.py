fc70aa7edc4c56ba3cfbf7e0c0c1ca622d91d486
Lord-Gusarov <2367@holbertonschool.com>
2020-12-10 12:41:36 -0400

    Making sure it wont execute when imported

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-names = dir(hidden_4)
-for x in names:
-    if x[:2] != "__":
-        print("{:s}".format(x))
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    for x in names:
+        if x[:2] != "__":
+            print("{:s}".format(x))
