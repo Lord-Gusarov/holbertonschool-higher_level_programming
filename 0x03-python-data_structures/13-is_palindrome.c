179b46f3a827855cb917f6ada9951adfeafe262f
Lord-Gusarov <2367@holbertonschool.com>
2020-12-15 17:23:44 -0400

    third version, now with a huge static array

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #define IS_PALINDROME 1
 #define NOT_PALINDROME -1
+#define ARR_SIZE 10000
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
@@ -10,48 +11,28 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *trv, *rev = NULL;
+	int cnt = 0, i;
+	listint_t *trv;
+	int rev[ARR_SIZE];
 
 	if (!head)
 		return (NOT_PALINDROME);
 	if (!*head)
 		return (IS_PALINDROME);
-
-	trv = *head;
 	while (trv)
 	{
-		add_node(&rev, trv->n);
+		rev[cnt++] = trv->n;
 		trv = trv->next;
 	}
 
-	trv = *head;
-	while (trv)
+	for (i = cnt - 1, trv = *head; i >= 0; i--, trv = trv->next)
 	{
-		if (trv->n != rev->n)
+		if (trv->n != rev[i])
 		{
-			free_listint(rev);
 			return (NOT_PALINDROME);
 		}
-		trv = trv->next;
-		rev = rev->next;
 	}
 
-	free_listint(rev);
 	return (IS_PALINDROME);
 }
 
-
-/**
- * add_node - adds a new node at the beggining of a list_t list
- * @head: adress current head of the list, will be updated if new node
- * is succesfully added at the beginning of a list
- * @n: value for the new node
- */
-void add_node(listint_t **head, int n)
-{
-	listint_t *new = malloc(sizeof(listint_t));
-
-	new->n = n;
-	new->next = *head;
-	*head = new;
-}
