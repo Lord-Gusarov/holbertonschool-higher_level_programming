e44fd048aa8f95927135c1575479aeeaeb7be630
Lord-Gusarov <2367@holbertonschool.com>
2020-12-15 12:18:55 -0400

    var name my_list was misspelled twice

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,5 +2,5 @@
 def replace_in_list(my_list, idx, element):
     l = len(my_list)
     if idx >= 0 and idx < l:
-        mylist[idx] = element
-    return mylist
+        my_list[idx] = element
+    return my_list
