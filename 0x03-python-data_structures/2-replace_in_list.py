9da04c78a0613f1bf89ae8c626a842a61a59fdb7
Lord-Gusarov <2367@holbertonschool.com>
2020-12-15 12:16:07 -0400

    Fixed a cmp of l with l, and shortened the code

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     l = len(my_list)
-    if idx < 0 or l >= l:
-        return my_list
-    mylist[idx] = element
+    if idx >= 0 and idx < l:
+        mylist[idx] = element
     return mylist
