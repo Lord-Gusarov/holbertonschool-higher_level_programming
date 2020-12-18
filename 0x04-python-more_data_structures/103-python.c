1f3c6d4cc92c8859c4cb1bab9c9332bb44c1fac3
Lord-Gusarov <2367@holbertonschool.com>
2020-12-17 22:15:48 -0400

    The order of function declaration and invoking matters

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,40 +2,13 @@
 #include <Python.h>
 #include <stdio.h>
 
-
 /**
- * print_python_list - prints some basic info about Python lists
- * @obj: Pointer to a python Object
-**/
-void print_python_list(PyObject *obj)
-{
-	int size, i;
-	PyObject *obj2;
-
-	printf("[*] Python list info\n");
-	size = (((PyVarObject *)(obj))->ob_size);
-	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %ld\n", ((PyListObject *)obj)->allocated);
-	for (i = 0; i < size; i++)
-	{
-		obj2 = (((PyListObject *)(obj))->ob_item[i]);
-		printf("Element %d: %s\n", i, ((obj2)->ob_type)->tp_name);
-		if (strcmp(((obj2)->ob_type)->tp_name, "bytes") == 0)
-		{
-			print_python_bytes(obj2);
-		}
-	}
-}
-
-
-/**
- *  print_python_bytes - prints info about Python bytes
+ * print_python_bytes - prints info about Python bytes
  * @obj: pointer to a object
-**/
+ */
 void print_python_bytes(PyObject *obj)
 {
-	int size;
-	int i = 0;
+	int size, i = 0;
 	char *str;
 
 	printf("[.] bytes object info\n");
@@ -61,3 +34,31 @@ void print_python_bytes(PyObject *obj)
 	}
 	printf("\n");
 }
+
+
+/**
+ * print_python_list - prints some basic info about Python lists
+ * @obj: Pointer to a python Object
+ */
+void print_python_list(PyObject *obj)
+{
+	int size, i;
+	PyObject *obj2;
+
+	printf("[*] Python list info\n");
+	size = (((PyVarObject *)(obj))->ob_size);
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %ld\n", ((PyListObject *)obj)->allocated);
+	for (i = 0; i < size; i++)
+	{
+		obj2 = (((PyListObject *)(obj))->ob_item[i]);
+		printf("Element %d: %s\n", i, ((obj2)->ob_type)->tp_name);
+		if (strcmp(((obj2)->ob_type)->tp_name, "bytes") == 0)
+		{
+			print_python_bytes(obj2);
+		}
+	}
+}
+
+
+
