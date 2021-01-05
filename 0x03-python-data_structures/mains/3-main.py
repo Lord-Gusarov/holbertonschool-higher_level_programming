db4f672644c0a82d7648249446b7720427168428
Lord-Gusarov <2367@holbertonschool.com>
2021-01-05 17:26:45 -0400

    full back-up of mains

--- 0x03-python-data_structures/mains/3-main.py
+++ 0x03-python-data_structures/mains/3-main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = [1, 2, 3, 4, 5]
+my_list = None
 print_reversed_list_integer(my_list)
-print(my_list)
+my_list = [1,2,3,4,5]
+print_reversed_list_integer(my_list)
+
--- 0x04-python-more_data_structures/mains/1-main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-search_replace = __import__('1-search_replace2').search_replace
-
-my_list = None
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
-
