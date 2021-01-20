65f7bfa196062f454763609cc6fa19ebaa9f685a
Lord-Gusarov <2367@holbertonschool.com>
2021-01-19 21:37:08 -0400

    adding task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Task: Improved Geometry
+"""
+
+
+class BaseGeometry():
+    """Empty class
+    """
+    def area(self):
+        """method not define yet
+        """
+        raise Exception('area() is not implemented')
