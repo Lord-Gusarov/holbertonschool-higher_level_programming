e34592b02b540f05cf1345a837bab7e1a203de6d
Lord-Gusarov <2367@holbertonschool.com>
2021-01-21 16:18:12 -0400

    adding task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Pascal Triangle
+"""
+
+def pascal_triangle(n):
+    """Return an (n) len Pascal triangle
+    """
+    if n < 1:
+        return []
+    r = []
+    l = [1]
+    for i in range(n):
+        r.append(l)
+        m = l[:] + [0]
+        idx = 1
+        for x in l:
+            m[idx] += x
+            idx += 1
+        l = m
+    return r
