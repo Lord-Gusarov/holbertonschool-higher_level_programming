fd0de951d2ebe092bf74862c49e8773424a7f624
Lord-Gusarov <2367@holbertonschool.com>
2021-01-24 09:27:19 -0400

    importing StringIO to before doing JSON load

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Task: From JSON string to Object
 """
+import json
+from io import StringIO
 
 
 def from_json_string(my_str):
@@ -10,5 +12,4 @@ def from_json_string(my_str):
     Returns:
         object: a Python data structure instance
     """
-    import json
-    return json.load(my_str)
+    return json.load(StringIO(my_str))
