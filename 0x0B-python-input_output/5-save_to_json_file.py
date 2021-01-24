b99bb6423b2a398aedf203f41720ccb7f16163fc
Lord-Gusarov <2367@holbertonschool.com>
2021-01-24 09:44:37 -0400

    adding task5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Save Object to a file
+"""
+import json
+
+
+def save_to_json_file(my_obt, filename):
+    """"Writes an Object to a text file, using a JSON representation
+    Args:
+        my_obt (any): a Python object that we want saved in a file
+        filename (str): name of the file to save/write the object to
+    """
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obt))
