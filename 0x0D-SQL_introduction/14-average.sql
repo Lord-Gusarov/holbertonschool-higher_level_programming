dce80953c181f3e82a2cf90625a69c3499bad22e
Gustavo Hornedo <gustavo.hc18@gmail.com>
2021-03-01 23:57:47 +0000

    Adds task #14

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Task: Write a script that computes the score average of all records in the table second_table
+-- Calculates the Avegare of the scores
+SELECT AVG(score) AS "average"
+FROM second_table;
