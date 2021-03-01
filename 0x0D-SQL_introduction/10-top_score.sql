795a43ab92d975c3e76d4970ceb34ea8d34ce86b
Gustavo Hornedo <gustavo.hc18@gmail.com>
2021-03-01 22:51:19 +0000

    Adds task #10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Task: List all records from second_table, showing just score and name.. ordered by score
+-- Selecting the records, the rows desired and how to order them
+SELECT score, name FROM second_table
+ORDER BY score DESC;
