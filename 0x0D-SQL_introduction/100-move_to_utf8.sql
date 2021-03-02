e07ab3c9bb8874f583365af396f9eb23e48dbb09
Gustavo Hornedo <gustavo.hc18@gmail.com>
2021-03-02 01:31:06 +0000

    Not depending on defaults, specifying collate too

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,14 @@
 -- Task: Write a script that converts hbtn_0c_0 database to UTF8
 -- Converting Database to utf8
 ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4;
--- Selecting database
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+--- Selecting database
 USE hbtn_0c_0;
 -- Converting table first_table to utf8
 ALTER TABLE first_table
-CHARACTER SET utf8mb4;
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 -- Converting field `name` in first_table
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
