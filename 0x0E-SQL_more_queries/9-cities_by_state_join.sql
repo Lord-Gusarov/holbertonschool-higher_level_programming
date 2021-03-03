a6c57856f73c8377434894151ef91a2d9daa8ac4
Gustavo Hornedo <gustavo.hc18@gmail.com>
2021-03-03 02:17:35 +0000

    Lets use INNER JOIN

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,7 @@
 -- Task: Write a script that lists all cities contained in the database hbtn_0d_usa.
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
-SELECT cities.id, cities.name, states.name*
-FROM cities NATURAL JOIN states
+SELECT cities.id, cities.name, states.name
+FROM cities INNER JOIN states
+ON cities.state_id = state.id
 ORDER BY cities.id;
