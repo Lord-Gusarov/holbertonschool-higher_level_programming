3cbfbab901895f04703e3933cf2585224eeae96d
Gustavo Hornedo <gustavo.hc18@gmail.com>
2021-03-03 02:16:51 +0000

    Specifyin output fields

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,6 @@
 -- Task: Write a script that lists all cities contained in the database hbtn_0d_usa.
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id
-SELECT *
-FROM cities
-NATURAL JOIN states
+SELECT cities.id, cities.name, states.name*
+FROM cities NATURAL JOIN states
 ORDER BY cities.id;
