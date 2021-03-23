0d9e96c71fd8f630f59180810f7db3511bceb683
Gustavo Hornedo <2367@holbertonschool.com>
2021-03-23 18:51:38 +0000

    Adds task #7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  while (x > 0) {
+    console.log('C is fun');
+    x--;
+  }
+}
