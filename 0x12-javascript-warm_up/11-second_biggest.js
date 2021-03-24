77afdd008bc6de3eab36c23cd1e38040e23ae5cc
Gustavo Hornedo <2367@holbertonschool.com>
2021-03-24 08:18:24 +0000

    Fixes output by first sorting the array in descending order

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-let idx, biggest;
-let second = 0;
+const args = process.argv.slice(2).map(Number);
+let output = 0;
 
 if (args.length > 1) {
-  for (idx = 0; idx < args.length; idx++) {
-    args[idx] = parseInt(args[idx]);
-  }
-  biggest = second = Math.min(...args);
-
-  for (idx = 0; idx < args.length; idx++) {
-    if (args[idx] >= biggest) {
-      second = biggest;
-      biggest = args[idx];
-    }
-  }
+  output = args.sort((a, b) => (b - a))[1];
 }
-
-console.log(second);
+console.log(output);
