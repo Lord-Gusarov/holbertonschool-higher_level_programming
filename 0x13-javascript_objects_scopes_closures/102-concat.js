eb4a7fa06646098916003e36818e6e54236d6734
Gustavo Hornedo <2367@holbertonschool.com>
2021-03-24 20:01:23 +0000

    Adds task #13 aka #102

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const files = process.argv.slice(2);
+
+try {
+  const content = fs.readFileSync(files[0]) +
+    fs.readFileSync(files[1]);
+  fs.writeFileSync(files[2], content);
+} catch {
+  console.log('Failed to concatenate the Files');
+}
