d46873f2a9a7d8df13e9ceed29c91c63108bd680
Gustavo Hornedo <2367@holbertonschool.com>
2021-03-27 16:17:11 +0000

    Chages the Comment style of the module

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# List all states from a specified database
+"""List all states from a specified database"""
 import sys
 import MySQLdb
 
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     user, passwd, db_name = sys.argv[1:4]
     db = MySQLdb.connect('localhost', user, passwd, db_name, 3306)
     cur = db.cursor()
-
     # Now we are ready for a query
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
