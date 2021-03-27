e5400fa4af7fecd4c450378f4f1362aae19eef6e
Gustavo Hornedo <2367@holbertonschool.com>
2021-03-27 16:31:53 +0000

    Using states.id to ORDER BY

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,15 +5,15 @@ import MySQLdb
 
 if __name__ == '__main__':
     user, passwd, db_name = sys.argv[1:4]
-    db = MySQLdb.connect('localhost', user, passwd, db_name, 3306)
+    db = MySQLdb.connect('localhost', user, passwd, db_name)
     cur = db.cursor()
     # Now we are ready for a query
     cur.execute("SELECT * FROM states "
                 "WHERE name LIKE 'N%' "
-                "ORDER BY id")
+                "ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
-        print("({}, '{}')".format(*row))
+        print(row)
     # Clean up
     cur.close()
     db.close()
