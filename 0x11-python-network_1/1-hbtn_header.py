099b61ca1ae0c5b4204f318afd1eee94bbe5f7b8
Gustavo Hornedo <2367@holbertonschool.com>
2021-04-13 11:47:07 +0000

    importing sys and not sys.argv

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,9 +3,9 @@
 Takes in a URL, sends a request to the URL and displays the value of the
 X-Request-Id variable found in the header of the response.
 """
-from sys import argv
+import sys
 import urllib.request
 
 
-with urllib.request.urlopen(argv[1]) as response:
+with urllib.request.urlopen(sys.argv[1]) as response:
     print(response.getheader('X-Request-Id'))
