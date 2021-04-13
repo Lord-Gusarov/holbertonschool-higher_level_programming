aae5a604ff761e7115f5ab98f45989063106b986
Gustavo Hornedo <2367@holbertonschool.com>
2021-04-13 11:52:44 +0000

    Removes extra space before each dash ('-') in the output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,5 +5,5 @@ import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-    o = "Body response:\n\t - type: {}\n\t - content: {}\n\t - utf8 content: {}"
+    o = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
     print(o.format(type(html), html, html.decode('utf8')))
