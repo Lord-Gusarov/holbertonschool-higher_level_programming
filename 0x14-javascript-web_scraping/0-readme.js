4d040af29d9b2f0afa9cf92b40a8e08eff1c7b49
Gustavo Hornedo <2367@holbertonschool.com>
2021-05-04 16:41:34 +0000

    Making it semistandard compliant

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', (err,data) => {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
-  console.log(data)
-})
+  console.log(data);
+});
