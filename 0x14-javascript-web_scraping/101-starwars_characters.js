05dce3b7f093f11fcfb8a39a01cc22dae4473093
Gustavo Hornedo <2367@holbertonschool.com>
2021-05-05 01:00:21 +0000

    Uses JS promises to make syncronous request and keep the Original Order of characters. Proyect 0x14

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,6 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
 
+function doRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, res, body) {
+      if (!error) {
+        resolve(JSON.parse(body).name);
+      } else {
+        reject(error);
+      }
+    });
+  });
+}
+
+async function main (film) {
+  for (const charAPI of film.characters) {
+    const res = await doRequest(charAPI);
+    console.log(res);
+  }
+}
+
 const films = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(films, (err, res, body) => {
   if (err) {
@@ -8,13 +27,5 @@ request(films, (err, res, body) => {
     return;
   }
   const film = JSON.parse(body);
-  for (const charAPI of film.characters) {
-    request(charAPI, (err, res, body) => {
-      if (err) {
-        console.error(err);
-      }
-      const character = JSON.parse(body);
-      console.log(character.name);
-    });
-  }
+  main(film);
 });
